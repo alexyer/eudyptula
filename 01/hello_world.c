38c402701614424552c25b3d7f4d20454cda592e
Olexander Yermakov <mannavard1611@gmail.com>
2014-08-27 00:10:51 +0300

    Added makefile

--- 01/hello_world.c
+++ 01/hello_world.c
@@ -1,19 +1,20 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("c70201c12db9");
+
 static int __init hello_world_init(void)
 {
 	pr_debug("Hello World!");
 	return 0;
 }
 
-static void __exit hello_world_initufs_exit(void)
+static void __exit hello_world_exit(void)
 {
-	pr_debug("Bye!");
+	pr_debug("Bye!\n");
 }
 
 module_init(hello_world_init);
-module_exit(hello_world_initufs_exit);
+module_exit(hello_world_exit);
 
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("c70201c12db9");
