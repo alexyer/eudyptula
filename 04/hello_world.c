aede73f8c8047d620dd1f1446b28edbe37994ea2
Olexander Yermakov <mannavard1611@gmail.com>
2014-08-30 10:57:15 +0300

    [c70201c12db9] Task 04: Fixed some coding style issues
    Fixed some coding style issues
    
    Signed-off-by: Olexander Yermakov <mannavard1611@gmail.com>

--- 04/hello_world.c
+++ 04/hello_world.c
@@ -6,8 +6,8 @@ MODULE_AUTHOR("c70201c12db9");
 
 static int __init hello_world_init(void)
 {
-        pr_debug("Hello World!");
-	return 0;  
+	pr_debug("Hello World!");
+	return 0;
 }
 
 static void __exit hello_world_exit(void)
