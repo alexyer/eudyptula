c14f0009bdac9408310c88874d43ed073625f6b9
Olexander Yermakov <mannavard1611@gmail.com>
2014-08-30 10:38:45 +0300

    Added task 04

---
+++ 04/hello_world.c
@@ -0,0 +1,20 @@
+#include <linux/init.h>
+#include <linux/module.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("c70201c12db9");
+
+static int __init hello_world_init(void)
+{
+        pr_debug("Hello World!");
+	return 0;  
+}
+
+static void __exit hello_world_exit(void)
+{
+	pr_debug("Bye!\n");
+}
+
+module_init(hello_world_init);
+module_exit(hello_world_exit);
+
