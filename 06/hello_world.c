694b1d7a8e0688f28b8e4fd3bc00bcbf68ba0fef
Olexander Yermakov <mannavard1611@gmail.com>
2014-09-09 23:48:24 +0300

    Rewrote task 06

--- 06/hello_world.c
+++ 06/hello_world.c
@@ -3,7 +3,7 @@
 #include <linux/fs.h>
 #include <linux/string.h>
 #include <linux/miscdevice.h>
-#include <asm/uaccess.h>
+#include <linux/uaccess.h>
 
 /*
  * Version information
@@ -16,10 +16,6 @@
 MODULE_LICENSE(DRIVER_LICENSE);
 MODULE_AUTHOR(DRIVER_AUTHOR);
 
-int init_module(void);
-void cleanup_module(void);
-static int device_open(struct inode *, struct file *);
-static int device_release(struct inode *, struct file *);
 static ssize_t device_read(struct file *, char *, size_t, loff_t *);
 static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 
@@ -27,18 +23,13 @@ static ssize_t device_write(struct file *, const char *, size_t, loff_t *);
 #define DEVICE_NAME "eudyptula"
 #define BUF_LEN 80
 #define EUDYPTULA_ID "c70201c12db9"
-#define DYNAMIC_MINOR 73
 
-static int major;
-static int dev_open = 0;
 static char msg[BUF_LEN];
-static char *msg_ptr;
 
-static struct file_operations fops = {
+static const struct file_operations fops = {
+	.owner = THIS_MODULE,
 	.read = device_read,
 	.write = device_write,
-	.open = device_open,
-	.release = device_release
 };
 
 static struct miscdevice my_dev;
@@ -46,17 +37,18 @@ static struct miscdevice my_dev;
 int init_module(void)
 {
 	int retval;
-	my_dev.minor = DYNAMIC_MINOR;
+
+	my_dev.minor = MISC_DYNAMIC_MINOR;
 	my_dev.name = DEVICE_NAME;
 	my_dev.fops = &fops;
 	retval = misc_register(&my_dev);
 
-	if (retval){
-		printk("error: %i", retval);
+	if (retval) {
+		pr_debug("error: %i", retval);
 		return retval;
 	}
 
-	printk("minor: %i", my_dev.minor);
+	pr_debug("minor: %i\n", my_dev.minor);
 
 	return 0;
 }
@@ -66,53 +58,41 @@ void cleanup_module(void)
 	misc_deregister(&my_dev);
 }
 
-static int device_open(struct inode *inode, struct file *file)
-{
-	if (dev_open)
-		return -EBUSY;
-	dev_open++;
-	sprintf(msg, EUDYPTULA_ID);
-	msg_ptr = msg;
-	try_module_get(THIS_MODULE);
-
-	return SUCCESS;
-}
-
-static int device_release(struct inode *inode, struct file *file)
-{
-	dev_open--;
-	module_put(THIS_MODULE);
-
-	return 0;
-}
-
 static ssize_t device_read(struct file *filp,
-         char *buffer,
-         size_t length,
-         loff_t * offset)
+	char *buffer,
+	size_t length,
+	loff_t *offset)
 {
-	int bytes_read = 0;
+	char *msg = EUDYPTULA_ID;
+	int len = strlen(msg);
 
-	if (*msg_ptr == 0)
+	if (length < len)
+		return -EINVAL;
+
+	if (*offset != 0)
 		return 0;
 
-	while (length && *msg_ptr) {
-		put_user(*(msg_ptr++), buffer++);
+	if (copy_to_user(buffer, msg, len))
+		return -EFAULT;
 
-		length--;
-		bytes_read++;
-	}
+	*offset = len;
 
-	return bytes_read;
+	return len;
 }
 
 static ssize_t
-device_write(struct file *filp, const char *buff, size_t len, loff_t * off)
+device_write(struct file *filp,
+	const char *buffer,
+	size_t length,
+	loff_t *offset)
 {
-	printk("%.*s", len, buff);
+	if (copy_from_user(msg, buffer, length))
+		return -EFAULT;
 
-	if (!strncmp((const char *)buff, EUDYPTULA_ID, strlen(EUDYPTULA_ID)))
-		return len;
-	else
-		return -EINVAL;
+	if (!strncmp(msg, EUDYPTULA_ID, strlen(EUDYPTULA_ID)) &&
+			length - 1 == strlen(EUDYPTULA_ID))
+		return length;
+
+	return -EINVAL;
 }
+
